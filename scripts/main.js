5f8f53b1f485bacb22441ac0a6c0c9266e9ac6ba
vm47197 <vm47197@ubt-uni.net>
2021-01-09 17:12:51 +0100

    Pre Final Commit v2

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 let mainNav = document.getElementById("js-menu");
 let navBarToggle = document.getElementById("js-navbar-toggle");
 
-navBarToggle.addEventListener("click", function () {
-    mainNav.classList.toggle("active");
-})
+// navBarToggle.addEventListener("click", function () {
+//     mainNav.classList.toggle("active");
+// })
 
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
